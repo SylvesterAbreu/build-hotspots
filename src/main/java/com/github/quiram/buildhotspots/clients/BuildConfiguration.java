5a94c61e27caf57439192b6264cc54f32cd71257
Sylvester Abreu <sylvester.abreu@digitaslbi.com>
2015-09-12 16:18:47 +0100

    Removing redundant tests at least they are at the moment

--- src/main/java/com/github/quiram/buildhotspots/clients/BuildConfiguration.java
+++ src/main/java/com/github/quiram/buildhotspots/clients/BuildConfiguration.java
@@ -2,7 +2,6 @@ package com.github.quiram.buildhotspots.clients;
 
 public class BuildConfiguration {
     private final String name;
-    private final BuildConfigurations buildConfigurations = new BuildConfigurations();
 
     public BuildConfiguration(String name) {
         this.name = name;
@@ -21,14 +20,5 @@ public class BuildConfiguration {
         BuildConfiguration otherBuild = BuildConfiguration.class.cast(o);
 
         return name.equals(otherBuild.name);
-    }
-
-    public boolean addBuildConfigurationDependency(BuildConfiguration buildConfiguration) {
-        buildConfigurations.add(buildConfiguration);
-        return true;
-    }
-
-    public boolean dependsOn(BuildConfiguration buildConfiguration) {
-        return buildConfigurations.contains(buildConfiguration);
     }
 }
