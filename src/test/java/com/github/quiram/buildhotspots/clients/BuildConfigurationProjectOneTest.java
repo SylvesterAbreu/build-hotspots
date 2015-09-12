04d510a562eb13de22cc933315da0ad3715fa45f
Sylvester Abreu <sylvester.abreu@digitaslbi.com>
2015-09-12 15:30:23 +0100

    Testing that ProjectOne depends on ProjectTwo

--- src/test/java/com/github/quiram/buildhotspots/clients/BuildConfigurationProjectOneTest.java
+++ src/test/java/com/github/quiram/buildhotspots/clients/BuildConfigurationProjectOneTest.java
@@ -14,4 +14,13 @@ public class BuildConfigurationProjectOneTest {
 
         assertTrue(projectOne.addBuildConfigurationDependency(projectTwo));
     }
+    
+    @Test
+    public void dependsOnProjectTwo() throws Exception {
+        
+        final BuildConfiguration projectOne = new BuildConfiguration("Project One");
+        final BuildConfiguration projectTwo = new BuildConfiguration("Project Two");
+        
+        assertTrue(projectOne.dependsOn(projectTwo));
+    }
 }
